88b6570675bdf6c68bcad329b332d6e3390cc642
chouayaSaif <saif.chouaya@tbs.u-tunis.tn>
2025-01-13 01:37:42 +0100

    using local csv file

--- src/main/java/com/saif/project/DataIngestion/ingestionType/CSVDataIngestion.java
+++ src/main/java/com/saif/project/DataIngestion/ingestionType/CSVDataIngestion.java
@@ -11,7 +11,12 @@ public class CSVDataIngestion implements DataIngestionInterface {
     @Override
     public Object importData(String filePath) throws IOException {
         List<String> data = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+        // Get the project directory path
+        String projectDirectory = System.getProperty("user.dir");
+        String fullPath = projectDirectory + File.separator + filePath;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fullPath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 data.add(line);
